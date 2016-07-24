298921a62d435ded85dfa1d4d60b48c27b6dc1b7
jaro <asd>
2016-07-24 02:19:05 +0200

    fixed logging

--- login_manager.py
+++ login_manager.py
@@ -11,11 +11,13 @@ lf = open(LOG_FILE, "a")
 def login(user, mac):
     users[mac] = user
     mac_list.write_file(ACTIVE_FILE, users)
-    lf.write("%s: %s logged in @%s" % (str(time.time()), user, mac))
+    lf.write("%.1f: %s logged in  @%s\n" % (time.time(), user, mac))
+    lf.flush()
 
 
 def logout(mac):
-    lf.write("%s: %s logged out @%s" % (str(time.time()), users[mac], mac))
+    lf.write("%.1f: %s logged out @%s\n" % (time.time(), users[mac], mac))
+    lf.flush()
     del users[mac]
     mac_list.write_file(ACTIVE_FILE, users)
 
