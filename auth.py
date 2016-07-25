5043583906e3318b7e86ab2011dd89b94da1b949
jaro <asd>
2016-07-25 17:42:12 +0200

    added ldap username support

--- auth.py
+++ auth.py
@@ -19,4 +19,14 @@ def check_credentials(user, passwd):
 
 
 def get_name(user):
-    return "Jaro"
+     ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
+
+    con = ldap.initialize("ldap://localhost:7389")
+    con.simple_bind_s('cn=admin,dc=sfn,dc=intranet', open("/etc/ldap.secret", "r").read())
+    try:
+        name = con.search_s('dc=sfn,dc=intranet', ldap.SCOPE_SUBTREE, 'uid=%s' % user)[0][1]['cn'][
+            0]
+    except IndexError:
+        name = None # user does not exist
+
+   return name
