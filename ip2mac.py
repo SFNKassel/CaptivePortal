86664d76a0e8f54b17e7fee6796468b2caea8527
jaro <asd>
2016-07-25 18:19:26 +0200

    ip2mac use dhcp lease

--- ip2mac.py
+++ ip2mac.py
@@ -1,15 +1,6 @@
-import netifaces as nif
+from isc_dhcp_leases import Lease, IscDhcpLeases
 
 
 def lookup(ip):
-    'Returns a list of MACs for interfaces that have given IP, returns None if not found'
-    for i in nif.interfaces():
-        address = nif.ifaddresses(i)
-        try:
-            if_mac = address[nif.AF_LINK][0]['addr']
-            if_ip = address[nif.AF_INET][0]['addr']
-        except(IndexError, KeyError):  # ignore ifaces that dont have MAC or IP
-            if_mac = if_ip = None
-        if if_ip == ip:
-            return if_mac
-    return None
+	leases = IscDhcpLeases('/path/to/dhcpd.leases')
+	return leases.get_current()[ip].ethernet 
